8325eeb2f2a159c80527386b60cc121630d3e9d2
Monthly Coffee <240860836+dlsduqdl1227-spec@users.noreply.github.com>
2026-07-07 13:42:05 +0900

    Remove old files

--- migrations/0003_registry.sql
+++
@@ -1,4 +0,0 @@
--- V7 registry/debriefing support. Safe to run multiple times where Cloudflare D1 allows IF NOT EXISTS.
-CREATE INDEX IF NOT EXISTS idx_participants_lookup ON participants(competition_code, name, phone);
-CREATE INDEX IF NOT EXISTS idx_participants_unit ON participants(competition_code, unique_no, cup_no, sample_no, team_no);
-CREATE INDEX IF NOT EXISTS idx_scores_unit ON scores(competition_code, unit, review_status);
